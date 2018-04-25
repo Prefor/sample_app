8320acf14d2eaf067f2ac13d9e33fc1d8fba24d4
Prefor <prefor11@gmail.com>
2018-04-25 23:58:24 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewApp::Application.config.secret_key_base = 'fd254648f6a289ed0db85018924fcebad7ee301179274337eb01455b401116872d33fbd7881d0d8983d57daf7461c64727d5ea46bdf37b1a68d0fe1a420db95b'
